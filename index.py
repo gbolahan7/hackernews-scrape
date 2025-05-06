689d308525465786bbce3a046e8771c14b5c0d89
Abass Durodola <abass_gbolahan@yahoo.com>
2025-05-06 09:27:31 +0100

    bs4 test extraction

--- index.py
+++ index.py
@@ -2,4 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+# print(soup.body)  #just to grab the body
+print(soup.body.contents)  #just to grab the body and content
+print(soup.find_all('a'))  #just to find all the links
