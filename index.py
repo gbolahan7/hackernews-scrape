7a81368fa94fbd93b394dd5e2f69adfeffe8e3e5
Abass Durodola <abass_gbolahan@yahoo.com>
2025-05-06 09:38:11 +0100

    extracting elements

--- index.py
+++ index.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-# print(soup.body)  #just to grab the body
-print(soup.body.contents)  #just to grab the body and content
-print(soup.find_all('a'))  #just to find all the links
+# # print(soup.body)  #just to grab the body
+# print(soup.body.contents)  #just to grab the body and content
+# print(soup.find_all('a'))  #just to find all the links
+
+
+# using selector
+
+# print(soup.select('.score')) # selecting class
+# print(soup.select('#score_1234')) # selecting id
+
+
+#Grabbing element with story link
+links = soup.select('.titleline')
+votes = soup.select('.score')
+# print(votes[0])
+print(votes[0].get('id'))
